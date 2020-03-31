8c37129a918fd70226b779df327abd9e6bd3dd8f
12264 <1226486517@qq.com>
2020-03-31 17:24:01 +0800

    重构

--- cloud-provider-payment8001/src/main/java/com/xjy/springcloud/controller/PaymentController.java
+++ cloud-provider-payment8001/src/main/java/com/xjy/springcloud/controller/PaymentController.java
@@ -7,16 +7,12 @@ import com.xjy.springcloud.entities.Payment;
 import com.xjy.springcloud.service.PaymentService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.*;
 
 import javax.annotation.Resource;
 
-@Controller
+@RestController
 @Slf4j
-@ResponseBody
 public class PaymentController {
 
     @Resource
@@ -24,7 +20,7 @@ public class PaymentController {
 
 
     @PostMapping(value = "/payment/create")
-    public CommonResult create(Payment payment){
+    public CommonResult create(@RequestBody Payment payment){
         int result = paymentService.create(payment);
         log.info("插入结果"+result);
         if (result > 0 ){
