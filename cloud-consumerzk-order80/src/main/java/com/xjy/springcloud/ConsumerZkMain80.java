c4b45a90b2ea5145764c7ccd3c567137b21ba56d
12264 <1226486517@qq.com>
2020-04-17 17:00:33 +0800

    zookeeper注册中心

---
+++ cloud-consumerzk-order80/src/main/java/com/xjy/springcloud/ConsumerZkMain80.java
@@ -0,0 +1,12 @@
+package com.xjy.springcloud;
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConsumerZkMain80 {
+    public static void main(String[] args) {
+        SpringApplication.run(ConsumerZkMain80.class,args);
+    }
+}
