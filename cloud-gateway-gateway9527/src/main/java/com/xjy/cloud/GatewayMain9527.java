b3af34c15cf74310a42106b1be797b07b53a7338
12264 <1226486517@qq.com>
2020-08-07 16:16:10 +0800

    gateway入门

---
+++ cloud-gateway-gateway9527/src/main/java/com/xjy/cloud/GatewayMain9527.java
@@ -0,0 +1,21 @@
+package com.xjy.cloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+/**
+ * @Author Salieri
+ * @Date 2020/8/7 14:11
+ * @Version 1.0
+ */
+
+
+@SpringBootApplication
+@EnableEurekaClient
+public class GatewayMain9527 {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GatewayMain9527.class,args);
+    }
+}
